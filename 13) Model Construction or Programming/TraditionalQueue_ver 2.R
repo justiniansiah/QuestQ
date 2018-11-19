5fb6d2709a43f26196648d68ba27f8376816c637
justiniansiah <justinian_siah@mymail.sutd.edu.sg>
2018-11-20 01:39:40 +0800

    updated R code. Can now output Avg Queue Length, Final Queue Length and Avg Wait Time

--- 13) Model Construction or Programming/TraditionalQueue_ver 2.R
+++ 13) Model Construction or Programming/TraditionalQueue_ver 2.R
@@ -14,7 +14,7 @@ randexpo <- function (lambda) {
   X <- -lambda*log(1-U)
   
   return(round(X))
-}
+  }
 
 
 ## Simulate 1 run to obtain mean queue length (and hopefully later on mean waiting time)
@@ -24,6 +24,10 @@ simulateOneRun <- function (QueueStart,Runtime,Interarrivals) {
   TotalCustomers = 0 #total customers served
   mean.Q = QueueStart
   var.Q = 0
+  
+  StartTime = c()
+  WaitTime = c()
+  
   
   arrival.flag = 0 #1 when system is waiting for customer, 0 otherwise
   service.flag = 0 #1 when system is processing a customer, 0 otherwise
@@ -35,14 +39,25 @@ simulateOneRun <- function (QueueStart,Runtime,Interarrivals) {
       TimetoCustomer = randexpo(Interarrivals)
       arrival.flag = 1
     }
+    
     #Add customer to queue if customer arrives
     if (TimetoCustomer == 0){
-      QueueLength = QueueLength + 1
-      arrival.flag = 0
+      QueueLength = QueueLength + 1  #For Average Queue Length Calculations, this value will go up and down
+      arrival.flag = 0               #Reset flag so check for next customer on next cycle
+      
+
+      StartTime = append(StartTime,CurrentTime) #Add in the current time to the list (for avg wait time calc)
+      
+      #This is to allow for multiple customers to come at once (this keeps time the same as we increment this at the end)
+      #If no customer come immediately (ie TimetoCustomer =/= 0 then the simulation proceeds as usual)
+      CurrentTime = CurrentTime - 1   
     }
+    #If not, decrease time taken for customer to arrive
     else{
       TimetoCustomer = TimetoCustomer - 1
     }
+    
+    #QueueTracker <<- append(QueueTracker,QueueLength) #For Graphing Purposes COMMENT OUT IF RUNNING MANY REPS
     
     ##Process People in Queue
     #Check if there is people in queue
@@ -59,7 +74,12 @@ simulateOneRun <- function (QueueStart,Runtime,Interarrivals) {
         {
           service.flag = 0 #reset flag
           QueueLength = QueueLength - 1 #release customer
-          TotalCustomers = TotalCustomers + 1
+          TotalCustomers = TotalCustomers + 1 #Increment total customers served (completed)
+          
+          #Here we use the time in the list "StartTime"
+          #We take the (current time - time stored) and put it into "WaitTime"
+          #If there people in the queue on initialisation, we ignore them 
+          WaitTime = append(WaitTime,(CurrentTime - StartTime[TotalCustomers-QueueStart]))
         }
         #if not done, decrease time till complete
         else{
@@ -78,28 +98,57 @@ simulateOneRun <- function (QueueStart,Runtime,Interarrivals) {
     CurrentTime = CurrentTime + 1
 
   }
-  #message("Average Length, Varience, Time passed (Min)")
-  #return(c(mean.Q,var.Q,CurrentTime/60))
-  return(mean.Q)
+  #Before we return, we calculate the mean of the waiting times for this simulation
+  AvgWait = sum(WaitTime)/TotalCustomers
+  
+  #Returns Avg Q Length, Current Q Length, Avg Waiting Time
+  return(c(mean.Q,QueueLength,AvgWait))
   #return(TotalCustomers)
 }
 
 simulatePeak <- function(){
   InitialQueue = round(simulateOneRun(0,30,arrival0)) #Initialise Queue length by running 1 simulation (non-peak, 30mins)
-  return(simulateOneRun(InitialQueue,45,arrival1))    #Use queue length to start off peak simulation (45mins)
+  Data = simulateOneRun(InitialQueue[2],45,arrival1)  #Use queue length to start off peak simulation (45mins)
+  return(Data)
 }
+#1 for Avg Queue Length
+#2 for Final Queue Length
+#3 for Avg Waiting Time
+
+#Conduct multiple replications of the simulations
+results <- replicate(100,simulatePeak())
+
+#display whole numbers for visual viewing.
+message("Average Queue Lengths")
+round(results[1,]) 
+message("Final Queue Lengths")
+round(results[2,]) 
+message("Average Waiting Time")
+round(results[3,]) 
+
+#See which parameter we are interested in (see above for which # to set)
+result.diff <- results[3,] 
 
 
-
-result.diff <- replicate(100,simulatePeak())
-round(result.diff)
+#Calculate Conf Interval
 ci.diff.rng <- t.test(result.diff,conf.level=0.95)$conf.int
 print(ci.diff.rng)
+mean(result.diff)
 
-### stuff below are for testing
-simulateOneRun()
+###################################
+### stuff below are for testing ###
+simulateOneRun(0,30,arrival0)
 replicate(100,randexpo(arrival1))
 
+
+#Graphs 
+#Uncomment the QueueTracker in line 60 (remember to comment back after done)
+QueueTracker = c() #clears vector
+#run 1 simulatePeak() ONLY 1 if not you will hang
+
+QueueTracker = QueueTracker[-c(1:1800)] #remove nonpeak
+plot(QueueTracker,type="l") #Full graph of 45mins
+plot(QueueTracker,type="l", xlim=c(1000,1600)) #a 5min window
 
 
 
