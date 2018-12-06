9bd5df884ae673f4db189b8620b172a7983b6250
BharatAtulDesai <36374471+BharatAD@users.noreply.github.com>
2018-12-06 22:53:03 +0800

    Grammar, clarity

--- 09) Data Collection/Data/SimulationData/data_viz.R
+++ 09) Data Collection/Data/SimulationData/data_viz.R
@@ -1,24 +1,28 @@
+
 nonpeak <- read.csv("C:/Users/bhara/Google Drive/SUTD/ESD T6/Simulation/project/QuestQ/09) Data Collection/Data/SimulationData/simulated_nonpeak.csv")
 peak <- read.csv("C:/Users/bhara/Google Drive/SUTD/ESD T6/Simulation/project/QuestQ/09) Data Collection/Data/SimulationData/simulated_peak.csv")
 leadpeak<- read.csv("C:/Users/bhara/Google Drive/SUTD/ESD T6/Simulation/project/QuestQ/09) Data Collection/Data/SimulationData/simulated_peak_lead.csv")
 leadnpeak<-read.csv("C:/Users/bhara/Google Drive/SUTD/ESD T6/Simulation/project/QuestQ/09) Data Collection/Data/SimulationData/simulated_nonpeak_lead.csv")
-
+modelcompare<-read.csv("C:/Users/bhara/Google Drive/SUTD/ESD T6/Simulation/project/QuestQ/09) Data Collection/Data/SimulationData/tandem.csv")
 require(reshape)
 nonpeak<-subset(nonpeak, select=c('simulated','observed'))
 peak<-subset(peak, select=c('simulated','observed'))
 leadnpeak<-subset(leadnpeak, select=c('simulated','observed'))
 leadpeak<-subset(leadpeak, select=c('simulated','observed'))
+modelcompare<-subset(modelcompare, select=c('tandem','fcfs'))
 
 nonpeak<-melt(nonpeak)
 peak<-melt(peak)
 leadnpeak<-melt(leadnpeak)
 leadpeak<-melt(leadpeak)
+modelcompare<-melt(modelcompare)
 
 
 nonpeak<-nonpeak[!is.na(nonpeak$value), ]
 peak<-peak[!is.na(peak$value), ]
 leadnpeak<-leadnpeak[!is.na(leadnpeak$value), ]
 leadpeak<-leadpeak[!is.na(leadpeak$value), ]
+modelcompare<-modelcompare[!is.na(modelcompare$value), ]
 
 
 require(ggplot2)
@@ -34,3 +38,6 @@ ggplot(leadnpeak,aes(x=value, color=variable))+geom_density()+
 
 ggplot(leadpeak,aes(x=value, color=variable))+geom_density()+
   theme_minimal()+labs(title="Densityplot of simulated and observed Peak lead times",color="Legend")
+
+ggplot(modelcompare,aes(x=value, color=variable))+geom_density()+
+  theme_minimal()+labs(title="Tandem vs FCFS Lead Times",color="Legend")
